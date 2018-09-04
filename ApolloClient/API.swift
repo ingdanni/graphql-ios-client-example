4cffc613924131b36fa7ed859ac6a3ab62cc39e7
Danny Narvaez <ing.danni@gmail.com>
2018-09-03 21:27:03 -0600

    Fetch all users.

--- ApolloClient/API.swift
+++ ApolloClient/API.swift
@@ -103,3 +103,105 @@ public final class FetchPostsQuery: GraphQLQuery {
     }
   }
 }
+
+public final class FetchUsersQuery: GraphQLQuery {
+  public let operationDefinition =
+    "query fetchUsers {\n  user {\n    __typename\n    id\n    name\n    username\n    email\n  }\n}"
+
+  public init() {
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes = ["RootQueryType"]
+
+    public static let selections: [GraphQLSelection] = [
+      GraphQLField("user", type: .list(.object(User.selections))),
+    ]
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(user: [User?]? = nil) {
+      self.init(unsafeResultMap: ["__typename": "RootQueryType", "user": user.flatMap { (value: [User?]) -> [ResultMap?] in value.map { (value: User?) -> ResultMap? in value.flatMap { (value: User) -> ResultMap in value.resultMap } } }])
+    }
+
+    public var user: [User?]? {
+      get {
+        return (resultMap["user"] as? [ResultMap?]).flatMap { (value: [ResultMap?]) -> [User?] in value.map { (value: ResultMap?) -> User? in value.flatMap { (value: ResultMap) -> User in User(unsafeResultMap: value) } } }
+      }
+      set {
+        resultMap.updateValue(newValue.flatMap { (value: [User?]) -> [ResultMap?] in value.map { (value: User?) -> ResultMap? in value.flatMap { (value: User) -> ResultMap in value.resultMap } } }, forKey: "user")
+      }
+    }
+
+    public struct User: GraphQLSelectionSet {
+      public static let possibleTypes = ["User"]
+
+      public static let selections: [GraphQLSelection] = [
+        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+        GraphQLField("id", type: .scalar(Int.self)),
+        GraphQLField("name", type: .scalar(String.self)),
+        GraphQLField("username", type: .scalar(String.self)),
+        GraphQLField("email", type: .scalar(String.self)),
+      ]
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public init(id: Int? = nil, name: String? = nil, username: String? = nil, email: String? = nil) {
+        self.init(unsafeResultMap: ["__typename": "User", "id": id, "name": name, "username": username, "email": email])
+      }
+
+      public var __typename: String {
+        get {
+          return resultMap["__typename"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      public var id: Int? {
+        get {
+          return resultMap["id"] as? Int
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "id")
+        }
+      }
+
+      public var name: String? {
+        get {
+          return resultMap["name"] as? String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "name")
+        }
+      }
+
+      public var username: String? {
+        get {
+          return resultMap["username"] as? String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "username")
+        }
+      }
+
+      public var email: String? {
+        get {
+          return resultMap["email"] as? String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "email")
+        }
+      }
+    }
+  }
+}
